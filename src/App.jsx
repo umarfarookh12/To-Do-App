ca999968ebb1d0d68e3b04bde52608327084aade
umar <sdumarfarookh212@gmail.com>
2024-08-21 12:09:22 +0530

    created simple onclick for api todo for adding in another card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,216 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import {
+  Box,
+  Checkbox,
+  Divider,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+} from "@mui/material";
+import CommentIcon from "@mui/icons-material/Comment";
+
+let todoApi = fetch("https://dummyjson.com/todos").then((res) => res.json());
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [test, setTest] = useState([]);
+  const [checked, setChecked] = React.useState([0]);
+  console.log(test);
+
+  useEffect(() => {
+    todoApi.then((data) => setTodos(data.todos));
+  }, []);
+
+  // const handleToggle = (value) => () => {
+  //   const currentIndex = checked.indexOf(value);
+  //   const newChecked = [...checked];
+
+  //   if (currentIndex === -1) {
+  //     newChecked.push(value);
+  //   } else {
+  //     newChecked.splice(currentIndex, 1);
+  //   }
+  //   setChecked(newChecked);
+  // };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+      spacing={1}
+    >
+      <Card
+        sx={{
+          width: 600,
+          height: "auto",
+          border: "1px solid #80cbc4",
+          borderRadius: "13px",
+        }}
+      >
+        <CardContent>
+          <div sx={{ padding: "2px" }}>
+            <Box sx={{ flexGrow: 1, padding: "2px" }}>
+              <Grid container spacing={2}>
+                <Grid item xs={10}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Tasks to be done.."
+                    variant="outlined"
+                    sx={{
+                      width: "100%",
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <Button
+                    sx={{ marginTop: "3px" }}
+                    size="large"
+                    color="warning"
+                    variant="contained"
+                  >
+                    Add
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+            <Divider sx={{ margin: "10px" }} />
+          </div>
+          <div>
+            <List
+              sx={{
+                width: "100%",
+              }}
+            >
+              {todos.map((value) => {
+                // const labelId = `checkbox-list-label-${value}`;
+
+                return (
+                  <ListItem
+                    key={value.id}
+                    secondaryAction={
+                      <IconButton edge="end" aria-label="comments">
+                        <CommentIcon />
+                      </IconButton>
+                    }
+                    disablePadding
+                  >
+                    <ListItemButton
+                      // role={undefined}
+                      // onClick={handleToggle(value)}
+                      dense
+                    >
+                      <ListItemIcon>
+                        <Checkbox
+                          edge="start"
+                          // checked={checked.indexOf(value) !== -1}
+                          // tabIndex={-1}
+                          // disableRipple
+                          // inputProps={{ "aria-labelledby": labelId }}
+                        />
+                      </ListItemIcon>
+                      <ListItemText
+                        // id={labelId}
+                        primary={`${value.todo}`}
+                        onClick={() => {
+                          const exists = test.findIndex((data) => {
+                            return data.id === value.id;
+                          });
+                          console.log(exists);
+                          if (exists === -1) {
+                            setTest([
+                              ...test,
+                              { id: value.id, todo: value.todo },
+                            ]);
+                          }
+                        }}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </div>
+        </CardContent>
+      </Card>
+      <Stack
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="center"
+        spacing={1}
+      >
+        {test.map((clicked) => (
+          <Box
+            key={clicked.userId}
+            component="section"
+            sx={{
+              p: 2,
+              width: 600,
+              height: "auto",
+              borderRadius: "13px",
+              margin: "1px",
+              border: "0.5px dashed grey",
+            }}
+          >
+            {clicked.todo}
+          </Box>
+        ))}
+      </Stack>
+      {/* <List
+        sx={{
+          width: "100%",
+        }}
+      >
+        {todos.map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem
+              key={value.id}
+              secondaryAction={
+                <IconButton edge="end" aria-label="comments">
+                  <CommentIcon />
+                </IconButton>
+              }
+              disablePadding
+            >
+              <ListItemButton
+                role={undefined}
+                onClick={handleToggle(value)}
+                dense
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText
+                  id={labelId}
+                  primary={`${value.todo}`}
+                  onClick={() => setTest([...test, value.todo])}
+                />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List> */}
+    </Stack>
+  );
 }
 
-export default App
+export default App;
