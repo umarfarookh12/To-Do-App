507cdd8890387cef7adf51d753df45a951b2f83d
umar <sdumarfarookh212@gmail.com>
2024-08-22 14:36:52 +0530

    "Added Redux toolkit and React Redux dependencies, updated package-lock.json and package.json, and modified main.jsx to include Redux store provider"

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,79 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
-import TextField from "@mui/material/TextField";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import {
+  Button,
   Checkbox,
   List,
   ListItem,
   ListItemButton,
-  ListItemIcon,
   ListItemText,
   Stack,
 } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos, slice } from "./store";
 
 let todoApi = fetch("https://dummyjson.com/todos").then((res) => res.json());
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [test, setTest] = useState([]);
-  let exists;
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    todoApi.then((data) => setTodos(data.todos));
-  }, []);
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  const handleToggle = (todo) => {
+    dispatch(slice.actions.toggleTodo(todo));
+  };
+
+  const todos = useSelector((state) => state.todos.todos);
+  const loading = useSelector((state) => state.todos.loading);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Card
       sx={{
-        width: 600,
         border: "0.5px solid #80cbc4",
         borderRadius: "13px",
       }}
     >
       <CardContent>
-        <TextField
-          label="Search for Tasks...."
-          variant="standard"
-          sx={{
-            width: "100%",
-          }}
-        />
-
-        {todos.map((value) => {
+        {todos.map((todo) => {
           return (
-            <List>
-              <ListItem
-                key={value.id}
-                disablePadding
-                onClick={() => {
-                  exists = test.findIndex((data) => {
-                    return data.id === value.id;
-                  });
-
-                  if (exists === -1) {
-                    setTest([
-                      ...test,
-                      {
-                        id: value.id,
-                        todo: value.todo,
-                        done: true,
-                      },
-                    ]);
-                  } else {
-                    alert("already addded");
-                  }
-                }}
+            <List disablePadding key={todo.id}>
+              <Stack
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+                spacing={1}
               >
-                <ListItemButton dense>
-                  <ListItemIcon>
-                    <Checkbox
-                      edge="start"
-                      checked={value.completed === true ? "checked" : false}
+                <ListItem
+                  key={todo.id}
+                  disablePadding
+                  onClick={() => {
+                    handleToggle(todo);
+                  }}
+                >
+                  <ListItemButton dense>
+                    <Checkbox edge="start" checked={todo.completed} />
+                    <ListItemText
+                      id={todo.id}
+                      primary={<span>{todo.todo}</span>}
+                      sx={{
+                        textDecoration: todo.completed ? "line-through" : null,
+                      }}
                     />
-                  </ListItemIcon>
-                  <ListItemText
-                    id={value.id}
-                    primary={
-                      <span
-                        style={{
-                          textDecoration: value.completed ? "line-through" : "",
-                        }}
-                      >
-                        {value.todo}
-                      </span>
-                    }
-                  />
-                </ListItemButton>
-              </ListItem>
+                  </ListItemButton>
+                </ListItem>
+                <Button color="warning" onClick={() => {}}>
+                  Delete
+                </Button>
+              </Stack>
             </List>
           );
         })}
